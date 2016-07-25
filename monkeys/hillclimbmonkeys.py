13e0743e64efc3fc72139c0498ed52f235ebfb25
Peter <peterju35@gmail.com>
2016-07-25 16:04:31 -0400

    Hillclimb monkeys now use strings, tidying and finishing up

--- monkeys/hillclimbmonkeys.py
+++ monkeys/hillclimbmonkeys.py
@@ -5,56 +5,62 @@
 import random
 
 def main():
-    goallist = ['m','e','t','h','i','n','k','s',' ','i','t',' ','i','s',' ','l','i','k','e',' ','a',' ','w','e','a','s','e','l']
+    goalstring = 'methinks it is like a weasel'
     bestscore = 0
-    beststring = ""
+    beststring = ''
     tries = 0
-    mylist = initialize()
-    print(goallist)
+    mystring = ''
+    print(goalstring)
 
-    while bestscore < 28:
-        mylist = generate(mylist)
-        score, mylist = compare(mylist, goallist)
+    while bestscore < len(goalstring):
+        mystring = generate(mystring, len(goalstring))
+        score, mystring = compare(mystring, goalstring)
         if score > bestscore:
             bestscore = score
-            beststring = mylist
-        if tries % 30 == 0:
-            print('TRY: ' + str(tries))
+            beststring = mystring
+        if tries % 10 == 0:
+            print('TRY #: ' + str(tries))
             print('BEST SCORE: ' + str(bestscore))
             print('BEST STRING: ' + str(beststring))
         tries += 1
-    print('Took ' + str(tries) + ' tries, to get ' + str(mylist))
 
-def initialize():
+    print('Took ' + str(tries) + ' tries, to get ' + str(mystring))
+
+# Takes in an input of a string
+# If string empty, generate a new string the size of the goalstring.
+# If string is filled, replace every 0 (wrong letter) with a new random letter
+# Returns the string
+def generate(myString, strLength):
     randompool = "abcdefghijklmnopqrstuvwxyz "
-    mylist = []
-    for i in range(0, 28):
-        letter = random.choice(randompool)
-        mylist.append(letter)
-
-    print(mylist)
-    return mylist
-
-def generate(mylist):
-    randompool = "abcdefghijklmnopqrstuvwxyz "
-    for i in range(0, len(mylist)):
-    	if mylist[i] ==  '0':
+    if myString == '':
+        for i in range(0, strLength):
             letter = random.choice(randompool)
-            mylist[i] = letter
+            myString += letter
+    else:
+        for i in range(0, strLength):
+    	    if myString[i] ==  '0':
+                letter = random.choice(randompool)
+                myString = myString[:i] + letter + myString[i+1:]
+                # mystring[i] = letter
 
-    # print(mystringlist)
-    return mylist
+    # print(mystring)
+    return myString
 
-def compare(generatedList, goalList):
+# Takes in the input of the generated string, and the string we want generated
+# If letters match, increment score
+# If letters don't match, replace letter with 0
+# Return the score and new string with 0s for wrong letters
+def compare(generatedString, goalString):
     score = 0
-    for i in range(0, len(generatedList)):
-        if generatedList[i] == goalList[i] and generatedList[i] != '0':
+    for i in range(0, len(generatedString)):
+        if generatedString[i] == goalString[i] and generatedString[i] != '0':
             score += 1
         else:
-            generatedList[i] = '0'
+        	generatedString = generatedString[:i] + '0' + generatedString[i+1:]
+            # generatedString[i] = '0'
   
     # print(score)
-    return(score, generatedList)
+    return(score, generatedString)
 
 if __name__ == '__main__':
     main()
