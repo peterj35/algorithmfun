581345ce1c9003862a6e85f9bea422d540987ee6
Peter <peterju35@gmail.com>
2016-07-27 14:13:37 -0400

    add Fractions practice

---
+++ Fraction/fraction.py
@@ -0,0 +1,69 @@
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+    return n
+
+def reduce(m,n):
+    common = gcd(m,n)
+
+    return m//common, n//common
+
+
+class Fraction:
+
+    def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
+
+    def __getitem__(self,key):
+        return 0
+
+    def __str__(self):
+        if self.num == 1 and self.den == 1:
+            return str(1)
+        if self.den == 1:
+            return str(self.num)
+
+        return str(self.num)+"/"+str(self.den)
+
+    def __eq__(self,other):
+        firstFraction = Fraction(*reduce(self.num, self.den))
+        secondFraction = Fraction(*reduce(other.num, other.den))
+        firstnum = firstFraction.num * secondFraction.den
+        secondnum = secondFraction.num * firstFraction.den
+
+        return firstnum == secondnum
+
+    def __add__(self,other):
+        newnum = self.num * other.den + self.den * other.num
+        newden = self.den * other.den
+
+        return Fraction(*reduce(newnum, newden))
+
+    def __sub__(self,other):
+        newnum = self.num * other.den - self.den * other.num
+        newden = self.den * other.den
+
+        return Fraction(*reduce(newnum, newden))
+
+    def __mul__(self,other):
+        newnum = self.num * other.num
+        newden = self.den * other.den
+
+        return Fraction(*reduce(newnum, newden))
+
+    def __truediv__(self,other):
+        newnum = self.num * other.den
+        newden = self.den * other.num
+
+        return Fraction(*reduce(newnum, newden))
+
+    def __floordiv__(self,other):
+        newnum = int(self.num * other.den)
+        newden = int(self.den * other.num)
+
+        return Fraction(*reduce(newnum, newden))
---
+++ Fraction/main.py
@@ -0,0 +1,48 @@
+from fraction import Fraction
+
+f1 = Fraction(3,4)
+f2 = Fraction(1,4)
+
+f3 = Fraction(5,4)
+f4 = Fraction(1,4)
+
+f5 = f1 + f2
+print("%s added by %s is %s." % (f1,f2,f5))
+
+f5 = f3 + f4
+print("%s added by %s is %s." % (f3,f4,f5))
+
+f5 = f1 - f2
+print("%s subtracted by %s is %s." % (f1,f2,f5))
+
+f5 = f3 - f4
+print("%s subtracted by %s is %s." % (f3,f4,f5))
+
+f5 = f1 * f2
+print("%s multiplied by %s is %s." % (f1,f2,f5))
+
+f5 = f3 + f4
+print("%s multiplied by %s is %s." % (f3,f4,f5))
+
+f5 = f1 / f2
+print("%s divided by %s is %s." % (f1,f2,f5))
+
+f5 = f3 / f4
+print("%s divided by %s is %s." % (f3,f4,f5))
+
+f1 = Fraction(14,5)
+f2 = Fraction(14,5)
+f3 = f1 == f2
+print("The statement '%s is equal to %s' is %s." % (f1,f2,f3))
+
+f1 = Fraction(1,2)
+f2 = Fraction(2,4)
+f3 = f1 == f2
+print("The statement '%s is equal to %s' is %s." % (f1,f2,f3))
+
+f1 = Fraction(1,3)
+f2 = Fraction(2,4)
+f3 = f1 == f2
+print("The statement '%s is equal to %s' is %s." % (f1,f2,f3))
+
+
